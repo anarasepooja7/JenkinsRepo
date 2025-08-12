6947cbcf89aa41a0b96bd2a52a7565dae09d7074
poojaanarase-123 <anarasepooja621@gmail.com>
2025-08-12 12:16:29 +0530

    new commit

--- src/test/java/demo/HelloCTS.java
+++ src/test/java/demo/HelloCTS.java
@@ -1,10 +1,10 @@
 package demo;
 
-public class HelloJava {
+public class HelloCTS {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub\
-		System.out.println("Hello Java");
+		System.out.println("Hello CTS ,Good Morning!!");
 
 	}
 
